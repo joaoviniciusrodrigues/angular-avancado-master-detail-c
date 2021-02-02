6a109b223379f6781b883aefee2a97c7575647a1
joaoviniciusrodrigues <32283342+joaoviniciusrodrigues@users.noreply.github.com>
2021-02-02 10:03:58 -0300

    improving category list component template

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
+  }
+
+  excluir(){
+    alert("Excluido");
   }
 
 }
