e8a51ef6d2df6fcb9e0f7280586f59b95b9c6bbb
joaoviniciusrodrigues <32283342+joaoviniciusrodrigues@users.noreply.github.com>
2021-02-03 17:58:08 -0300

    project restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDatabase } from "./in-memory-database";
+import { CoreModule } from "./core/core.module"
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
-    HttpClientModule
+  imports: [    
+    CoreModule,
+    AppRoutingModule,    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
