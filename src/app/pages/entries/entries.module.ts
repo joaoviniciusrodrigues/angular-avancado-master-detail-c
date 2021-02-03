0b9a2efc7dbd3da3a996033b779b8168373add65
joaoviniciusrodrigues <32283342+joaoviniciusrodrigues@users.noreply.github.com>
2021-02-03 09:39:33 -0300

    adding EntryListComponent

---
+++ src/app/pages/entries/entries.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EntriesRoutingModule } from './entries-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {EntryListComponent} from './entries-list/entry-list.component'
+
+
+
+@NgModule({
+  declarations: [EntryListComponent],
+  imports: [
+    CommonModule,
+    EntriesRoutingModule,
+    ReactiveFormsModule
+  ]
+})
+export class EntriesModule { }
